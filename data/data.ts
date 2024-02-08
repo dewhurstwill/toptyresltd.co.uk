7bd8b3a36f58c41c0d5abd31e38dd3bffc4bea0b
Will Dewhurst <34936646+dewhurstwill@users.noreply.github.com>
2024-02-08 13:12:53 +0000

    Update data.ts

--- data/data.ts
+++ data/data.ts
@@ -5,7 +5,7 @@ const data = {
     title: "Coming Soon",
     description: "We are cooking up a storm, check back soon.",
     newsletterheading: "Stay tuned for our live updates!",
-    copyrightText: 'Copyright © 2024 TopTyresLtd - Website By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://devvy.co.uk">DEVVY</a>',
+    copyrightText: 'Copyright © 2024 TopTyresLtd - Website By <a target="_blank" class="no-underline md:underline" href="https://devvy.co.uk">DEVVY</a>',
     socialIconsHeading: "Follow Us 📣",
     socialIcons: [
     ],

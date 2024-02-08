125ed631d6d53b1851c1c3768a130eb5dc28f2ac
Will Dewhurst <34936646+dewhurstwill@users.noreply.github.com>
2024-02-08 12:52:14 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,29 +51,9 @@ export default function Home() {
           </p>
         </div>
 
-      <div className='text-center lg:m-7 mt-10 w-80 p-3' >
-          <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label htmlFor="email" className="block text-slate-800 dark:text-slate-100  font-light text-sm leading-6">{newsletterheading}</label>
-              <div className="mt-2 flex-col flex lg:flex md:flex-row">
-                <input id="email" name="email" type="email" placeholder='Email address' autoComplete="email" required className="block w-full placeholder:text-gray-500 pl-[10px] focus:outline-none border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mr-0 rounded-none p-2" />
-                <button className='ml-0 bg-slate-900 sm:w-auto border-slate-800 dark:border-slate-100	rounded-none mt-2 md:mt-0	p-2 border-2 dark:text-white hover:bg-slate-950'>Subscribe</button>
-              </div>
-            </div>
-          </form>
-        </div>
-
         <footer className='text-slate-500 text-center'>
           <div className='my-4 text-center' >
             <ul className='flex flex-wrap lg:flex justify-center '>
-              <li className='px-2'> {socialIconsHeading} </li>
-              {socialIcons.map((social, index) =>
-              (
-                <li key={index} className='px-2 capitalize border-1  hover:border-b-white'>
-                  <a target='_blank' className='' href={social.link}> {social.icon}  </a>
-                </li>
-              )
-              )}
             </ul>
           </div>
           <div className=''>
